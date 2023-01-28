d6770da9213b1c4120d7998a1e47672bb43def9c
Saiful182 <saifulislamcse0@gamil.com>
2023-01-28 22:08:49 +0600

    all done

--- JS/depositeWithdraw.js
+++ JS/depositeWithdraw.js
@@ -1,29 +1,52 @@
 document.getElementById('deposite-btn').addEventListener('click', function () {
     const inputDeposite = document.getElementById('input-deposite');
     const inputDepositevalue = parseFloat(inputDeposite.value);
+    if (isNaN(inputDepositevalue)) {
+        alert('Please Type a valid Number');
+        inputDeposite.value = '';
+    }
+    else {
+        const priviousDeposite = document.getElementById('privious-deposite');
+        const priviousDepositeValue = parseFloat(priviousDeposite.innerText);
+
+        const newDeposite = priviousDepositeValue + inputDepositevalue
+        priviousDeposite.innerText = newDeposite.toFixed(2)
 
 
-    const priviousDeposite = document.getElementById('privious-deposite');
-    const priviousDepositeValue = parseFloat(priviousDeposite.innerText);
+        const priviousBalance = document.getElementById('privious-balance');
+        const priviousBalanceValue = parseFloat(priviousBalance.innerText);
+        const newBalance = priviousBalanceValue + inputDepositevalue;
 
-    const newDeposite = priviousDepositeValue + inputDepositevalue
-    priviousDeposite.innerText = newDeposite.toFixed(2)
+        priviousBalance.innerText = newBalance.toFixed(2);
 
-
-    const priviousBalance = document.getElementById('privious-balance');
-    const priviousBalanceValue = parseFloat(priviousBalance.innerText);
-    console.log(typeof (priviousBalanceValue));
-    const newBalance = priviousBalanceValue + inputDepositevalue;
-
-    priviousBalance.innerText = newBalance.toFixed(2);
-
-    inputDeposite.value = '';
-
+        inputDeposite.value = '';
+    }
 
 })
 
-document.getElementById('withdraw-btn').addEventListener('click'), function () {
+document.getElementById('withdraw-btn').addEventListener('click', function () {
+
     const inputWithdraw = document.getElementById('input-withdraw');
-    const depositevalue = parseFloat(inputWithdraw.value);
-    inputWithdraw.value = '';
-}
+    const inputWithdrawValue = parseFloat(inputWithdraw.value);
+
+    if (isNaN(inputWithdrawValue)) {
+        alert('Please Type a valid Number');
+        inputWithdraw.value = '';
+    }
+    else {
+        const priviousWithdraw = document.getElementById('privious-withdraw');
+        const priviousWithdrawValue = parseFloat(priviousWithdraw.innerText);
+
+        const newWithdraw = priviousWithdrawValue + inputWithdrawValue;
+        priviousWithdraw.innerText = newWithdraw.toFixed(2)
+
+
+        const priviousBalance = document.getElementById('privious-balance');
+        const priviousBalanceValue = parseFloat(priviousBalance.innerText);
+
+        const newBalance = priviousBalanceValue - inputWithdrawValue;
+        priviousBalance.innerText = newBalance.toFixed(2);
+
+        inputWithdraw.value = '';
+    }
+})
