f8a16d6daa3eb5ce825cf8dfff62f75159c42d4f
Saiful182 <saifulislamcse0@gamil.com>
2023-01-28 21:39:06 +0600

    deposite part done

--- JS/depositeWithdraw.js
+++ JS/depositeWithdraw.js
@@ -1,7 +1,24 @@
 document.getElementById('deposite-btn').addEventListener('click', function () {
     const inputDeposite = document.getElementById('input-deposite');
-    const depositevalue = parseFloat(inputDeposite.value);
+    const inputDepositevalue = parseFloat(inputDeposite.value);
+
+
+    const priviousDeposite = document.getElementById('privious-deposite');
+    const priviousDepositeValue = parseFloat(priviousDeposite.innerText);
+
+    const newDeposite = priviousDepositeValue + inputDepositevalue
+    priviousDeposite.innerText = newDeposite.toFixed(2)
+
+
+    const priviousBalance = document.getElementById('privious-balance');
+    const priviousBalanceValue = parseFloat(priviousBalance.innerText);
+    console.log(typeof (priviousBalanceValue));
+    const newBalance = priviousBalanceValue + inputDepositevalue;
+
+    priviousBalance.innerText = newBalance.toFixed(2);
+
     inputDeposite.value = '';
+
 
 })
 
